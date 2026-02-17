7588b6090d57555fef8165c215e7b46efe655c0b
rbhosale575 <rbhosale895@gmail.com>
2026-02-17 13:30:11 +0530

    Add kiss creation functionality on interval

--- script.js
+++ script.js
@@ -2,4 +2,17 @@
 document.getElementById("start").style.display="none";
 document.getElementById("main").style.display="block";
 document.getElementById("sound").play();
- }
+
+setInterval(createKiss,300);
+}
+
+function createKiss(){
+let kiss=document.createElement("div");
+kiss.classList.add("kiss");
+kiss.innerHTML="💋";
+document.body.appendChild(kiss);
+
+setTimeout(()=>{
+kiss.remove();
+},1500);
+}
