31593734120152e39894c072f7f6c82dacafe5cf
rbhosale575 <rbhosale895@gmail.com>
2026-02-17 13:40:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
  function startProject(){
+
 document.getElementById("start").style.display="none";
 document.getElementById("main").style.display="block";
 document.getElementById("sound").play();
 
-setInterval(createKiss,300);
+startKisses();
+}
+
+function startKisses(){
+setTimeout(createKiss,100);
 }
 
 function createKiss(){
+
 let kiss=document.createElement("div");
 kiss.classList.add("kiss");
 kiss.innerHTML="💋";
@@ -15,4 +21,6 @@ document.body.appendChild(kiss);
 setTimeout(()=>{
 kiss.remove();
 },1500);
+
+setTimeout(createKiss,200); // next kiss
 }
