d94d38e33be70870d51af39b3e631aba52a26715
rbhosale575 <rbhosale895@gmail.com>
2026-02-17 13:13:59 +0530

    Refactor script.js to remove styles and add startProject function
    Removed CSS styles for body and kiss class, added startProject function to manage UI display and play sound.

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-body{
-perspective:1000px;
-overflow:hidden;
-}
-
-.kiss{
-position:fixed;
-top:50%;
-left:50%;
-transform:translate(-50%,-50%) scale(0.1);
-font-size:40px;
-animation:zoomKiss 2s linear forwards;
-}
-
-@keyframes zoomKiss{
-0%{
-transform:translate(-50%,-50%) scale(0.1);
-opacity:0;
-}
-20%{
-opacity:1;
-}
-100%{
-transform:translate(-50%,-50%) scale(4);
-opacity:0;
-}
-  }
+ function startProject(){
+document.getElementById("start").style.display="none";
+document.getElementById("main").style.display="block";
+document.getElementById("sound").play();
+ }
