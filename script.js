25d35cd187355673038e24a0cb69fa649b9d3a2c
rbhosale575 <rbhosale895@gmail.com>
2026-02-17 14:00:32 +0530

    Refactor startProject to use setTimeout for kisses
    Replaced kissInterval with a loop and setTimeout to create kisses at intervals.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
- let kissInterval;
-
-function startProject(){
+ function startProject(){
 
 document.getElementById("start").style.display="none";
 document.getElementById("main").style.display="block";
 document.getElementById("sound").play();
 
-kissInterval = setInterval(createKiss, 250);
+for(let i=0;i<20;i++){
+setTimeout(createKiss,i*100);
+}
+
 }
 
 function createKiss(){
@@ -14,11 +15,11 @@ function createKiss(){
 let kiss=document.createElement("div");
 kiss.className="kiss";
 kiss.innerHTML="💋";
-document.body.appendChild(kiss);
 
-// kiss sound
-let ks = new Audio("https://assets.mixkit.co/active_storage/sfx/2571/2571-preview.mp3");
-ks.play();
+kiss.style.left=Math.random()*100+"vw";
+kiss.style.top=Math.random()*100+"vh";
+
+document.body.appendChild(kiss);
 
 setTimeout(function(){
 kiss.remove();
